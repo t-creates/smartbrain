1468eb88d332188f2a7070644cc51792e25ba257
Travis Geislinger <travisgeislinger@Traviss-MacBook-Pro.local>
2022-03-20 15:29:19 -0600

    Commit #5 Starting face recognition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation.jsx';
 import Logo from './components/Logo/Logo.jsx';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.jsx'
@@ -6,7 +7,9 @@ import Rank from './components/Rank/Rank.jsx'
 import ParticlesBG from "./components/Particles/ParticlesBG.jsx";
 import './App.css';
 
-
+const app = new Clarifai.App({
+  apiKey: '44568a297a0f4119b0f6e64d93aec392'
+});
 
 class App extends Component {
   constructor() {
@@ -22,6 +25,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log('CLICK');
+    app.models.predict('44568a297a0f4119b0f6e64d93aec392', 'https://samples.clarifai.com/face-det.jpg').then(
+      function (response) {
+        console.log(response);
+      },
+      function (error) {
+
+      }
+    )
   }
 
   render() {
